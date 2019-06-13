05b3ca402c454bcd7508aeeb8679b71982d2c112
Achint Satsangi <achint.satsangi@gmail.com>
2019-06-13 13:03:09 +0200

    Using components in components... getting started

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,23 +17,22 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
   render() {
+    var product = { id: 1 };
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <br />
-        {this.state.tags.length === 0 && "Please create new tags!"}
-        {this.renderTags()}
       </div>
     );
   }
