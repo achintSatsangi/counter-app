c385341117ea5d3991d52ed14de84613fdf86959
Achint Satsangi <achint.satsangi@gmail.com>
2019-06-13 13:39:30 +0200

    Removed tags properties, also added association between counters and counter objects

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
-    tags: []
+    value: this.props.counter.value
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   handleIncrement = () => {
     this.setState({ value: this.state.value + 1 });
@@ -26,10 +14,16 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
