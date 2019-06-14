2ff3f7a7f4cce01a565c51623a312cad82c51779
Achint Satsangi <achint.satsangi@gmail.com>
2019-06-14 09:10:07 +0200

    Disable decrement if counter is zero or less

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -24,6 +24,7 @@ class Counter extends Component {
         <button
           onClick={() => this.props.onDecrement(this.props.counter)}
           className="btn btn-secondary btn-sm m-2"
+          disabled={this.props.counter.value < 1}
         >
           -
         </button>
@@ -38,8 +39,8 @@ class Counter extends Component {
   }
 
   getBadgeClasses() {
-    var classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    var classes = "badge badge-";
+    classes += this.props.counter.value === 0 ? "warning m-2" : "primary m-3";
     return classes;
   }
 
