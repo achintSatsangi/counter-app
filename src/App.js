699695aa1c7d4fdacce9423b6f9c1c8d0d4aef24
Achint Satsangi <achint.satsangi@gmail.com>
2019-06-14 08:52:51 +0200

    Added a possibility of decrement as well

--- src/App.js
+++ src/App.js
@@ -37,6 +37,14 @@ class App extends Component {
     this.setState({ counters });
   };
 
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
   handleReset = () => {
     const counters = this.state.counters.map(c => {
       c.value = 0;
@@ -58,6 +66,7 @@ class App extends Component {
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
             onDelete={this.handleDelete}
+            onDecrement={this.handleDecrement}
           />
         </main>
       </React.Fragment>
